4f408b75490fe41f661c35d2e220aae527c6d9e7
fargox-dev <fargox.dev@gmail.com>
2025-01-24 15:44:22 -0600

    Update index.js

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+b1 = document.getElementById("articulaciones-btn");
+b1.addEventListener("click", () => {
+    window.location.href = "./articulaciones.html";
+})
+
+b2 = document.getElementById("cancer-btn");
+b2.addEventListener("click", () => {
+    window.location.href = "cancer.html";
+})
+
+b3 = document.getElementById("diabetes-btn");
+b3.addEventListener("click", () => {
+    window.location.href = "diabetes.html";
+})
+
+b4 = document.getElementById("dialisis-btn");
+b4.addEventListener("click", () => {
+    window.location.href = "dialisis.html";
+})
+
+b5 = document.getElementById("higadograso-btn");
+b5.addEventListener("click", () => {
+    window.location.href = "higadograso.html";
+})
+
+b6 = document.getElementById("parkinson-btn");
+b6.addEventListener("click", () => {
+    window.location.href = "parkinson.html";
+})
+
+b7 = document.getElementById("tiroides-btn");
+b7.addEventListener("click", () => {
+    window.location.href = "tiroides.html";
+})
+
