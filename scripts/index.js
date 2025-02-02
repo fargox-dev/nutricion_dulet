830283c992330b0d3c6d3dc59162237e99f2ee82
000web <fr4ker49@gmail.com>
2025-02-01 23:05:46 -0600

    Finished Product 1.0
    Upload finished product version 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -38,7 +38,190 @@ github.addEventListener("click", () => {
     window.location.href = "https://github.com/fargox-dev";
 })
 
+
+
+
+
+
 ficha = document.getElementById("ficha-btn");
 ficha.addEventListener("click", () => {
     window.location.href = "./ficha.html";
+})
+
+beneficios = document.getElementById("beneficios-btn");
+beneficios.addEventListener("click", () => {
+    window.location.href = "./beneficios.html";
+})
+
+comotomarlo = document.getElementById("comotomarlo-btn");
+comotomarlo.addEventListener("click", () => {
+    window.location.href = "./comotomarlo.html";
+})
+
+
+
+
+
+
+
+
+
+
+anemia = document.getElementById("anemia-btn");
+anemia.addEventListener("click", () => {
+    window.location.href = "./anemia.html";
+})
+    
+
+ansiedad = document.getElementById("ansiedad-btn");
+ansiedad.addEventListener("click", () => {
+    window.location.href = "./ansiedad.html";
+})
+    
+
+arterosclerosis = document.getElementById("arterosclerosis-btn");
+arterosclerosis.addEventListener("click", () => {
+    window.location.href = "./arterosclerosis.html";
+})
+
+artritis = document.getElementById("artritis-btn");
+artritis.addEventListener("click", () => {
+    window.location.href = "./artritis.html";
+})
+    
+
+autismo = document.getElementById("autismo-btn");
+autismo.addEventListener("click", () => {
+    window.location.href = "./autismo.html";
+})
+    
+
+bulimia = document.getElementById("bulimia-btn");
+bulimia.addEventListener("click", () => {
+    window.location.href = "./bulimia.html";
+})
+    
+
+calambres = document.getElementById("calambres-btn");
+calambres.addEventListener("click", () => {
+    window.location.href = "./calambres.html";
+})
+    
+
+    
+
+deporte = document.getElementById("deporte-btn");
+deporte.addEventListener("click", () => {
+    window.location.href = "./deporte.html";
+})
+    
+
+depresion = document.getElementById("depresion-btn");
+depresion.addEventListener("click", () => {
+    window.location.href = "./depresion.html";
+})
+    
+
+dermatitis = document.getElementById("dermatitis-btn");
+dermatitis.addEventListener("click", () => {
+    window.location.href = "./dermatitis.html";
+})
+    
+
+desnutricion = document.getElementById("desnutricion-btn");
+desnutricion.addEventListener("click", () => {
+    window.location.href = "./desnutricion.html";
+})
+    
+    
+
+fatiga = document.getElementById("fatiga-btn");
+fatiga.addEventListener("click", () => {
+    window.location.href = "./fatiga.html";
+})
+
+
+gastritis = document.getElementById("gastritis-btn");
+gastritis.addEventListener("click", () => {
+    window.location.href = "./gastritis.html";
+})
+    
+
+glutation = document.getElementById("glutation-btn");
+glutation.addEventListener("click", () => {
+    window.location.href = "./glutation.html";
+})
+    
+
+hemodialisis = document.getElementById("hemodialisis-btn");
+hemodialisis.addEventListener("click", () => {
+    window.location.href = "./hemodialisis.html";
+})
+    
+
+hepatitis = document.getElementById("hepatitis-btn");
+hepatitis.addEventListener("click", () => {
+    window.location.href = "./hepatitis.html";
+})
+    
+
+herpes = document.getElementById("herpes-btn");
+herpes.addEventListener("click", () => {
+    window.location.href = "./herpes.html";
+})
+
+inflamacion = document.getElementById("inflamacion-btn");
+inflamacion.addEventListener("click", () => {
+    window.location.href = "./inflamacion.html";
+})
+    
+insomnio = document.getElementById("insomnio-btn");
+insomnio.addEventListener("click", () => {
+    window.location.href = "./insomnio.html";
+})
+    
+
+migraña = document.getElementById("migraña-btn");
+migraña.addEventListener("click", () => {
+    window.location.href = "./migraña.html";
+})
+    
+
+nerviosismo = document.getElementById("nerviosismo-btn");
+nerviosismo.addEventListener("click", () => {
+    window.location.href = "./nerviosismo.html";
+})
+    
+
+obesidad = document.getElementById("obesidad-btn");
+obesidad.addEventListener("click", () => {
+    window.location.href = "./obesidad.html";
+})
+    
+
+osteoporosis = document.getElementById("osteoporosis-btn");
+osteoporosis.addEventListener("click", () => {
+    window.location.href = "./osteoporosis.html";
+})  
+
+quimioterapia = document.getElementById("quimioterapia-btn");
+quimioterapia.addEventListener("click", () => {
+    window.location.href = "./quimioterapia.html";
+})
+    
+
+terceraedad = document.getElementById("terceraedad-btn");
+terceraedad.addEventListener("click", () => {
+    window.location.href = "./terceraedad.html";
+})
+
+ulceras = document.getElementById("ulceras-btn");
+ulceras.addEventListener("click", () => {
+    window.location.href = "./ulceras.html";
+})
+    
+
+vitiligo = document.getElementById("vitiligo-btn");
+vitiligo.addEventListener("click", () => {
+    window.location.href = "./vitiligo.html";
 })
